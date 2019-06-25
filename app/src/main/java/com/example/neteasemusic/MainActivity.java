0414aa223101a8930094d213275ffbd2bcf79068
LiangAnHua <810175373@qq.com>
2019-06-25 08:31:18 +0800

    首次提交

---
+++ app/src/main/java/com/example/neteasemusic/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.neteasemusic;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.neteasemusic.Adapter.MyPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
